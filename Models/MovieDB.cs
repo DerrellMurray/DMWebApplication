d9792f2629861e1e5e5a074cf1048c64707335c4
Derrell Murray <Derrell.Murray@outlook.com>
2019-08-09 12:11:08 -0400

    Add project files.

---
+++ Models/MovieDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using DMWebApplication.Models;
+
+namespace DMWebApplication.Models
+{
+    public class MovieDB
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+
+    public class MovieDBContext : System.Data.Entity.DbContext
+    {
+        public DbSet<MovieDB> Movies { get; set; }
+    }
+}
